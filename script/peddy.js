52d3f464c635b3798c35f2d02f99a494425bfcfb
Emon <abamin3091@gmail.com>
2026-05-16 01:01:21 +0600

    added adopt button modal

--- script/peddy.js
+++ script/peddy.js
@@ -1,37 +1,11 @@
-// this function is for all categories like dogs cats rabbits ...
-
-// card demo ={
-//   "status": true,
-//   "message": "successfully fetched all the categories data",
-//   "categories": [
-//     {
-//       "id": 1,
-//       "category": "Cat",
-//       "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
-//     },
-//     {
-//       "id": 2,
-//       "category": "Dog",
-//       "category_icon": "https://i.ibb.co.com/c8Yp1y7/dog.png"
-//     },
-//     {
-//       "id": 3,
-//       "category": "Rabbit",
-//       "category_icon": "https://i.ibb.co.com/3hftmLC/rabbit.png"
-//     },
-//     {
-//       "id": 4,
-//       "category": "Bird",
-//       "category_icon": "https://i.ibb.co.com/6HHZwfq/bird.png"
-//     }
-//   ]
-// }
 
 
-// Category Button
+
+
+
 
 const loadCategories=()=>{
-    fetch `https://openapi.programming-hero.com/api/peddy/categories`
+    fetch (`https://openapi.programming-hero.com/api/peddy/categories`)
     .then(res => res.json())
     .then(data => displayDetails(data.categories))
     .catch(error => console.log(error))
@@ -41,10 +15,7 @@ const displayDetails=(categories)=>{
     // console.log(categories);
     const categoryContainer = document.getElementById('animals')
  categories.forEach(items => {
-    // console.log(items)
-
-//('flex','gap-4','font-bold','border-teal-600','border','px-11','py-3','rounded-xl'
-        // ,'text-gray-700','hover:bg-teal-100','transition',
+   
 
    const buttonContainer = document.createElement('div')
      buttonContainer.classList.add('flex','items-center','text-center','justify-center','border','w-[110px]','border-gray-400','gap-2',
@@ -69,11 +40,10 @@ const loadPets = (id) => {
    // for spinner show
       document.getElementById('spinner').classList.remove('hidden')
     // alert(id)
-
-
+ 
     // for 2 second time 
     setTimeout(()=>{
-
+ 
 
       //for hidden spinner
       document.getElementById('spinner').classList.add('hidden')
@@ -90,101 +60,156 @@ const loadPets = (id) => {
 }
 
 
+sortPriceBtn=(data)=>{
+   data.sort((a , b)=> b.price-a.price)
+   detailsVideo(data)
+}
 
 
 
 
 const loadVideos = ()=>{
-    fetch`https://openapi.programming-hero.com/api/peddy/pets`
+  let AllPet =[];
+    fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
     .then(res=> res.json())
-    .then(data=>detailsVideo(data.pets))
-    .catch(error=>console.log(error))
+    .then(data=> {AllPet = data.pets; detailsVideo(AllPet)})
+
+  
+
+    .catch(error=>console.log(error));
+    document.getElementById('Sort').addEventListener('click',function(){
+       sortPriceBtn(AllPet)
+      });
 }
 
 
 const detailsVideo=(pets)=>{
-    const videoContainer = document.getElementById('videos')
-    videoContainer.innerHTML="";
 
-    if(pets.length==0){
-    videoContainer.classList.remove('grid');
-    videoContainer.innerHTML=
-   
+
+
+        
+
+                const videoContainer = document.getElementById('videos')
+                videoContainer.innerHTML="";
+              
+                if(pets.length==0){
+                videoContainer.classList.remove('grid');
+                videoContainer.innerHTML=
+               
+                
+                `
+                <div class="bg-gray-300 border rounded-2xl">
+                <div class="w-30px flex justify-center pt-20">
+                <img class=w-200"" src="assets/error.webp">
+               </div>  
+                 <h1 class="font-bold flex justify-center text-2xl py-4">No Information Available</h1>  
+                 <p class="text-gray-700 flex justify-center pb-20">It is a long established fact that a reader will be distracted by the readable content of a <br> page when looking at 
+              its layout.The point of using Lorem Ipsum is that it has a.</p>
+                 <div/>
+               `
+              
+                }
+                else{
+                        videoContainer.classList.add('grid');
+                }
+                // console.log(pets)
+                pets.forEach(pets=>{
+                    // console.log(pets)
+              
+              
+              
+                    // create a card 
+              
+                     const card = document.createElement('div')
+                     card.classList = 'card bg-base-100 w-75 shadow-sm'
+                     card.innerHTML =`
+                      <figure class="px-3 pt-3  overflow-hidden">
+                <img class="rounded-xl  w-80 h-48   object-cover"
+                  src=${pets.image}
+                  alt=""
+                  class="" />
+              </figure>
+              <div class="card-body">
+                <h2 class="font-bold text-xl">${pets.pet_name}</h2>
+               <div>
+               <div class="flex items-center gap-2">
+               <img class="w-5 text-gray-500 rounded-full  " src ="https://img.icons8.com/?size=100&id=ETI89AT9Xeqo&format=png&color=000000"/>
+               <p class="text-gray-500">Breed: ${pets.breed}</p>
+               </div>
+               <div class="flex items-center gap-2">
+               <img class="w-6 text-gray-500" src="https://img.icons8.com/?size=100&id=wSNPBOvl6qCE&format=png&color=000000" />
+               <p class="text-gray-500">${pets.date_of_birth}</p>
+               </div>
+               <div class="flex gap-2">
+               <img class="w-5 text-gray-500 py-1"  src="https://img.icons8.com/?size=100&id=QUuwkaxmcF9h&format=png&color=000000"/>
+               <p class="text-gray-500">Gender: ${pets.gender}</p>
+               </div>
+               <div class="flex gap-2 pb-5 border-b border-gray-300">
+               <img class="w-5 text-gray-500" src="https://img.icons8.com/?size=100&id=85801&format=png&color=000000"/>
+               <p class="text-gray-500  border-blue-700">Price:199$</p>
+              
+               
+               </div>
+               <div class="flex gap-7 mt-5">
+               <button onclick="loadBtn('${pets.petId}')" class="btn w-15 rounded-xl  "><img class="w-5" src="https://img.icons8.com/?size=100&id=u8MTpAq972MG&format=png&color=000000"/></button>
+                <button class="btn w-18 rounded-xl  hover:bg-cyan-700 hover:text-white text-cyan-500 font-bold" onclick="loadAdoptDetails()">Adopt</button>
+                 <button class="btn w-18 rounded-xl hover:bg-cyan-700 hover:text-white text-cyan-500 font-bold" onclick="loadDetails(${pets.petId})" >Details</button>
+                 </div>
+                 
+               </div>
+              </div> `
+              
+              
+                videoContainer.append(card)
+                });
+
+
     
-    `
-    <div class="bg-gray-300 border rounded-2xl">
-    <div class="w-30px flex justify-center pt-20">
-    <img class=w-200"" src="assets/error.webp">
-   </div>  
-     <h1 class="font-bold flex justify-center text-2xl py-4">No Information Available</h1>  
-     <p class="text-gray-700 flex justify-center pb-20">It is a long established fact that a reader will be distracted by the readable content of a <br> page when looking at 
-its layout.The point of using Lorem Ipsum is that it has a.</p>
-     <div/>
-   `
 
-    }
-    else{
-            videoContainer.classList.add('grid');
-    }
-    // console.log(pets)
-    pets.forEach(pets=>{
-        // console.log(pets)
-
-
-
-        // create a card 
-
-         const card = document.createElement('div')
-         card.classList = 'card bg-base-100 w-75 shadow-sm'
-         card.innerHTML =`
-          <figure class="px-3 pt-3  overflow-hidden">
-    <img class="rounded-xl  w-80 h-48   object-cover"
-      src=${pets.image}
-      alt=""
-      class="" />
-  </figure>
-  <div class="card-body">
-    <h2 class="font-bold text-xl">${pets.pet_name}</h2>
-   <div>
-   <div class="flex items-center gap-2">
-   <img class="w-5 text-gray-500 rounded-full  " src ="https://img.icons8.com/?size=100&id=ETI89AT9Xeqo&format=png&color=000000"/>
-   <p class="text-gray-500">Breed: ${pets.breed}</p>
-   </div>
-   <div class="flex items-center gap-2">
-   <img class="w-6 text-gray-500" src="https://img.icons8.com/?size=100&id=wSNPBOvl6qCE&format=png&color=000000" />
-   <p class="text-gray-500">${pets.date_of_birth}</p>
-   </div>
-   <div class="flex gap-2">
-   <img class="w-5 text-gray-500 py-1"  src="https://img.icons8.com/?size=100&id=QUuwkaxmcF9h&format=png&color=000000"/>
-   <p class="text-gray-500">Gender: ${pets.gender}</p>
-   </div>
-   <div class="flex gap-2 pb-5 border-b border-gray-300">
-   <img class="w-5 text-gray-500" src="https://img.icons8.com/?size=100&id=85801&format=png&color=000000"/>
-   <p class="text-gray-500  border-blue-700">Price:199$</p>
-  
-   
-   </div>
-   <div class="flex gap-7 mt-5">
-   <button onclick="loadBtn('${pets.petId}')" class="btn w-15 rounded-xl  "><img class="w-5" src="https://img.icons8.com/?size=100&id=u8MTpAq972MG&format=png&color=000000"/></button>
-    <button class="btn w-18 rounded-xl text-cyan-500 font-bold">Adopt</button>
-     <button class="btn w-18 rounded-xl hover:bg-cyan-700 hover:text-white text-cyan-500 font-bold" onclick="loadDetails(${pets.petId})" >Details</button>
-     </div>
-     
-   </div>
-  </div> `
-
-
-    videoContainer.append(card)
-    });
-
-  
 
 }
 
 
+// for adop button
+
+const loadAdoptDetails=()=>{
+  let count = 3;
+
+const adoptButtonContainer =  document.getElementById('adopt-card-body')
+adoptButtonContainer.innerHTML =`
+<div class=" flex flex-col items-center">
+<img class="   w-10 h-10" src='https://img.icons8.com/?size=100&id=cVpPwvEfF3Bn&format=png&color=000000'/>
+<h1 class="text-3xl font-bold">Congrates</h1>
+<h5 class=" py-3">Adoption Process is Start for your pets</h5>
+<span id="countdown-number" class="font-mono font-bold text-6xl">3</span>
+</div>
+
+`
 
 
+document.getElementById('CountDownModal').showModal()
 
+
+const interval = setInterval(()=>{
+ count--;
+ if(count <=0){
+  clearInterval(interval)
+  document.getElementById('CountDownModal').close();
+ }else{
+  document.getElementById('countdown-number').innerText= count;
+ }
+}, 1000);
+
+}
+// const interval = setInterval(() => {
+//     count--;
+//     if(count <= 0){
+//         clearInterval(interval);
+//         document.getElementById('CountDownModal').close();
+//     } else {
+//         document.getElementById('countdown-number').innerText = count;
+//     }
+// }, 1000);
 
 
 
@@ -296,6 +321,8 @@ loadPicture=(petData)=>{
 
 
 
+
+//  show spinner
 
 
 
